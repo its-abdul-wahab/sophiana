9cb4c828f65231ae7bd1323e134a3b80937ef8d5
saqibjaved786 <saqibjaved107f@gmail.com>
2023-12-06 19:26:39 +0500

    fixes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ import { useLocation } from "react-router-dom";
 
 const navigation = {
   main: [
-    { name: "+ 1-212-345-6789", href: "#" },
-    { name: "Email: team@Sophiana.com", href: "#" },
-    { name: "LOS ANGELES   HONG KONG ", href: "#" },
+    { name: "+ 1-212-345-6789" },
+    { name: "Email: team@Sophiana.com" },
+    { name: "LOS ANGELES   HONG KONG " },
   ],
 };
 
@@ -28,13 +28,10 @@ export default function Footer() {
         <div className="max-w-[662px] px-4 mx-auto py-5 sm:pt-12">
           <div className="flex items-center justify-between flex-col sm:flex-row sm:py-12 px-8 sm:border-t border-primary">
             {navigation.main.map((item) => (
-              <div key={item.name} className="">
-                <a
-                  href={item.href}
-                  className="text-xs sm:text-[10px] font-inter-light tracking-[2px]"
-                >
+              <div key={item.name}>
+                <span className="text-xs sm:text-[10px] font-inter-light tracking-[2px]">
                   {item.name}
-                </a>
+                </span>
               </div>
             ))}
           </div>
